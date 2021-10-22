bd2831acbc62d7cb0cfe628bbba20e577e6c6523
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-10-21 18:03:58 -0700

    Change some things

--- src/main/java/io/github/blobanium/mineclubexpanded/MineclubExpanded.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/MineclubExpanded.java
@@ -1,14 +1,14 @@
-package net.fabricmc.example;
+package io.github.blobanium.mineclubexpanded;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class MineclubExpanded implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("Mineclub Expanded");
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Mineclub Expanded Initialized!");
 	}
 }
---
+++ src/main/java/io/github/blobanium/mineclubexpanded/mixin/ChatHudListenerMixin.java
@@ -0,0 +1,37 @@
+package io.github.blobanium.mineclubexpanded.mixin;
+
+import io.github.blobanium.mineclubexpanded.MineclubExpanded;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHudListener;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Mixin(ChatHudListener.class)
+public class ChatHudListenerMixin {
+    private final Pattern outbidPattern = Pattern.compile("You have been outbid by");
+
+    @Inject(at = @At("HEAD"), method = "onChatMessage")
+    public void onChatMessage(MessageType messageType, Text message, UUID sender, CallbackInfo ci){
+        Matcher outbidMatcher = outbidPattern.matcher(message.getString());
+        if(outbidMatcher.matches()){
+            System.out.println("Triggered message");
+            SystemToast toast = SystemToast.create(MinecraftClient.getInstance(), SystemToast.Type.TUTORIAL_HINT, new LiteralText("ayyy!!!"), new LiteralText("It Worked!"));
+            MinecraftClient.getInstance().getToastManager().add(toast);
+        } else {
+            System.out.println(outbidPattern);
+            System.out.println(outbidMatcher);
+        }
+
+    }
+}
