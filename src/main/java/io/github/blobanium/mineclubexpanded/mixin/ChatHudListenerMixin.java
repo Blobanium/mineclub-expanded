5cf0e83dec50883104f66b2a4c669f9d174948e2
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-10-29 19:30:00 -0700

    Remove info LOGGER, document some stuff

--- src/main/java/io/github/blobanium/mineclubexpanded/mixin/ChatHudListenerMixin.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/mixin/ChatHudListenerMixin.java
@@ -24,15 +24,16 @@ public class ChatHudListenerMixin {
 
     @Inject(at = @At("HEAD"), method = "onChatMessage")
     public void onChatMessage(MessageType messageType, Text message, UUID sender, CallbackInfo ci){
+        //Detect If A Chat message is being deleted on mineclub.
         if(message.getString().matches("ꌄ§7")){
             msgCount = msgCount + 1;
             if(msgCount >= 80){
                 chatBeingCleared = true;
-                MineclubExpanded.LOGGER.info("chat being cleared");
             }
         } else if(!chatBeingCleared) {
             lastMessage2 = lastMessage;
             lastMessage = message.getString();
+            //Broadcast On Chat Message
             OutbidNotifier.onChatMessage(message);
         } else {
             if(lastMessage.equals(message.getString())||lastMessage2.equals(message.getString())){
