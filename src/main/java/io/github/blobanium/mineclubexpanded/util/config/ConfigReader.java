7acd9b47ec78822e62eafd8a8bda6cc239623ff2
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-10-30 16:09:25 -0700

    ... why didnt it fully push

---
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/config/ConfigReader.java
@@ -0,0 +1,45 @@
+package io.github.blobanium.mineclubexpanded.util.config;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+public class ConfigReader {
+	//variables
+	public static boolean refreshingConfig = false;
+
+	//configs
+	public static boolean outbidNotification = false;
+
+    public static final Logger LOGGER = LogManager.getLogger("Mineclub Expanded");
+
+    public static void configRegister(){
+    	LOGGER.debug("Registering config..");
+    	SimpleConfig CONFIG = SimpleConfig.of("MineclubExpanded").provider(namespace -> ConfigReader.ltProvider(namespace)).request();
+    	final boolean outbidConfig = CONFIG.getOrDefault("outbid_notification", outbidNotification);
+    }
+
+    private static String ltProvider(String filename) {
+    	return "#Mineclub Expanded Config File."
+    	+ "\noutbid_notification=" + outbidNotification;
+    }
+
+	public static void refreshConfig(){
+		refreshingConfig = true;
+		try {
+			if(!Files.deleteIfExists(FabricLoader.getInstance().getConfigDir().resolve("MineclubExpanded.properties"))){
+				LOGGER.error("Config file not found. Please ensure the path to the config is correct.\n" + FabricLoader.getInstance().getConfigDir().resolve("LoadingTimer.properties"));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			LOGGER.fatal("Config Refresh Failed due to a IOException, please report this on our issues thread.");
+			e.printStackTrace();
+		}
+		configRegister();
+		refreshingConfig = false;
+	}
+}
