a4a53e7376f4626ade5d2cc3dcea0575759c9c39
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-10-29 19:12:17 -0700

    public github alpha testing

--- src/main/java/io/github/blobanium/mineclubexpanded/mixin/ChatHudListenerMixin.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/mixin/ChatHudListenerMixin.java
@@ -1,5 +1,6 @@
 package io.github.blobanium.mineclubexpanded.mixin;
 
+import io.github.blobanium.mineclubexpanded.MineclubExpanded;
 import io.github.blobanium.mineclubexpanded.market.OutbidNotifier;
 import net.minecraft.client.gui.hud.ChatHudListener;
 import net.minecraft.network.MessageType;
@@ -16,9 +17,27 @@ import java.util.regex.Pattern;
 public class ChatHudListenerMixin {
     //TODO: Get The regex to work
     private final Pattern outbidPattern = Pattern.compile("ꌄ\\[Market] You have been outbid by");
+    private static String lastMessage;
+    private static String lastMessage2;
+    private static Integer msgCount = 0;
+    private static boolean chatBeingCleared = false;
 
     @Inject(at = @At("HEAD"), method = "onChatMessage")
     public void onChatMessage(MessageType messageType, Text message, UUID sender, CallbackInfo ci){
-        OutbidNotifier.onChatMessage(message);
+        if(message.getString().matches("ꌄ§7")){
+            msgCount = msgCount + 1;
+            if(msgCount >= 80){
+                chatBeingCleared = true;
+                MineclubExpanded.LOGGER.info("chat being cleared");
+            }
+        } else if(!chatBeingCleared) {
+            lastMessage2 = lastMessage;
+            lastMessage = message.getString();
+            OutbidNotifier.onChatMessage(message);
+        } else {
+            if(lastMessage.equals(message.getString())||lastMessage2.equals(message.getString())){
+                chatBeingCleared = false;
+            }
+        }
     }
 }
--- src/main/java/io/github/blobanium/mineclubexpanded/util/SoundPlayer.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/SoundPlayer.java
@@ -11,12 +11,12 @@ import net.minecraft.util.registry.Registry;
 public class SoundPlayer {
     public static void playSound(){
         SoundEvent sound = registerSound();
-        PositionedSoundInstance posSound = PositionedSoundInstance.master(sound, 1.0F);
+        PositionedSoundInstance posSound = PositionedSoundInstance.master(sound, 1.0F, 2.0F);
         MinecraftClient.getInstance().getSoundManager().play(posSound);
     }
 
     private static SoundEvent registerSound(){
-        Identifier soundId = new Identifier(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP.getId().toString());
+        Identifier soundId = new Identifier(SoundEvents.ENTITY_BEE_DEATH.getId().toString());
         return Registry.SOUND_EVENT.get(soundId);
     }
 }
