fcfec9a26d566942069c454bd00646aa817c45fd
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-11-05 11:47:04 -0700

    Remove comment
    Autogg fully works now

--- src/main/java/io/github/blobanium/mineclubexpanded/games/AutoGG.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/games/AutoGG.java
@@ -8,8 +8,6 @@ import net.minecraft.util.Identifier;
 
 public class AutoGG {
     public static void autoGg(SoundInstance sound){
-        //NOT WORKING YET
-        //Note: only works for Battle Dome And Dodge Ball
         if(ConfigReader.autogg) {
             if (sound.getId().toString().equals("minecraft:custom.mineclub.roundover-1")) {
                 MinecraftClient.getInstance().player.sendChatMessage("gg");
