df6f0806344b631656393bd9b5523cd98fa6d030
Blobanium <45613796+Blobanium@users.noreply.github.com>
2021-11-11 10:26:25 -0800

    Comment for every game, Add Speed Tag Support

--- src/main/java/io/github/blobanium/mineclubexpanded/global/WorldListener.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/global/WorldListener.java
@@ -3,6 +3,7 @@ package io.github.blobanium.mineclubexpanded.global;
 import io.github.blobanium.mineclubexpanded.MineclubExpanded;
 import io.github.blobanium.mineclubexpanded.util.discord.DiscordRP;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
 
 public class WorldListener {
     private static String worldName;
@@ -12,7 +13,7 @@ public class WorldListener {
             try {
                 if (!MinecraftClient.getInstance().world.getRegistryKey().getValue().getPath().equals(worldName)) {
                     worldName = MinecraftClient.getInstance().world.getRegistryKey().getValue().getPath();
-                    MineclubExpanded.LOGGER.debug("WorldName=" + worldName);
+                    System.out.println("WorldName=" + worldName);
                     worldCheck(worldName);
                 }
             } catch (NullPointerException e) {
@@ -22,52 +23,69 @@ public class WorldListener {
     }
 
     private static void worldCheck(String world){
+        //Lobby, AFK Lounge
         if(world.equals("overworld")){
             DiscordRP.updateStatus("In The Lobby", "Playing On Mineclub");
         }
 
+        //Battle Dome
         if(world.equals("gamemap_battle_dome")){
             DiscordRP.updateStatus("Currently In Battle Dome", "Playing On Mineclub");
         }
 
+        //Slime Walls
         if(world.equals("gamemap_slime_walls")){
             DiscordRP.updateStatus("Currently In Slime Walls", "Playing On Mineclub");
         }
 
+        //Laser Tag
         if(world.equals("gamemap_laser_tag")){
             DiscordRP.updateStatus("Currently In Laser Tag", "Playing On Mineclub");
         }
 
+        //Dodgeball
         if(world.equals("gamemap_dodge_ball")){
             DiscordRP.updateStatus("Currently In Dodge Ball", "Playing On Mineclub");
         }
 
+        //ANY Housing Map
         if(world.startsWith("housing")){
             DiscordRP.updateStatus("Currently In Housing", "Playing On Mineclub");
         }
 
+        //Connect4
         if(world.startsWith("connect4")){
             DiscordRP.updateStatus("Currently Playing Connect 4", "Playing On Mineclub");
         }
 
+        //Match5
         if(world.startsWith("match5")){
             DiscordRP.updateStatus("Currently Playing Match 5", "Playing On Mineclub");
         }
 
+        //Lucky Shot
         if(world.startsWith("luckyshot")){
             DiscordRP.updateStatus("Currently Playing Lucky Shot", "Playing On Mineclub");
         }
 
+        //Tic Tac Toe
         if(world.startsWith("ttt")){
             DiscordRP.updateStatus("Currently Playing Tic Tac Toe", "Playing On Mineclub");
         }
 
+        //Sumo
         if(world.startsWith("sumo")){
             DiscordRP.updateStatus("Currently Playing Sumo", "Playing On Mineclub");
         }
 
+        //Minesweep
         if(world.startsWith("ms")){
             DiscordRP.updateStatus("Currently Playing Minesweep", "Playing On Mineclub");
+        }
+
+        //Speedtag
+        if(world.startsWith("master")){
+            DiscordRP.updateStatus("Currently Playing Speed Tag", "Playing On Mineclub");
         }
     }
 }
