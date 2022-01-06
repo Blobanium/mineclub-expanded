8521cf79e123a6b4d7bff451698edc71e709bfc6
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-01-06 12:57:37 -0800

    make it use 1 string instead of 2.

--- src/main/java/io/github/blobanium/mineclubexpanded/util/mixinhelper/InventoryItemIdentifier.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/mixinhelper/InventoryItemIdentifier.java
@@ -5,15 +5,13 @@ import net.minecraft.item.Item;
 import net.minecraft.screen.slot.Slot;
 
 public class InventoryItemIdentifier {
-    private static String jsonDisplayName;
     private static String fullname;
 
     public static void checkItem(Slot slot){
         //Player Heads have a Raw ID of 955
         if(slot != null){
             if(Item.getRawId(slot.getStack().getItem()) == 955 && slot.getStack().getNbt().getCompound("display").getString("Name").length() != 0) {
-                jsonDisplayName = slot.getStack().getNbt().getCompound("display").getString("Name");
-                fullname = jsonDisplayName.substring(130).replace("\"}],\"text\":\"\"}", "");
+                fullname = slot.getStack().getNbt().getCompound("display").getString("Name").substring(130).replace("\"}],\"text\":\"\"}", "");
                 if(fullname.length() <= 16){
                     HousingRichPresenceListener.playerheadName = fullname;
                 } else {
