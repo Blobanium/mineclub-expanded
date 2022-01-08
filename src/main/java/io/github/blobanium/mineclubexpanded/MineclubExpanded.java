16d500f312eeab3b0c769369b3d47aef80bfc1fe
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-01-07 16:21:21 -0800

    Do not start RP if on Mac OS

--- src/main/java/io/github/blobanium/mineclubexpanded/MineclubExpanded.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/MineclubExpanded.java
@@ -27,11 +27,16 @@ public class MineclubExpanded implements ModInitializer {
 		// Proceed with mild caution.
 
 		ConfigReader.configRegister();
-		try {
-			DiscordRP.startRP();
-		} catch (Exception e){
-			LOGGER.error("Failed to start rich presence, Your Device/Install may not support rich presence! \n" + e);
+		if(!MinecraftClient.IS_SYSTEM_MAC) {
+			try {
+				DiscordRP.startRP();
+			} catch (Exception e) {
+				LOGGER.error("Failed to start rich presence, Your Device/Install may not support rich presence! \n" + e);
+				DiscordRP.supportsRichPresence = false;
+			}
+		} else {
 			DiscordRP.supportsRichPresence = false;
+			LOGGER.error("Rich Presence doesn't support macOS.");
 		}
 		mineclub.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.ENABLED);
 		hasInitialized = true;
