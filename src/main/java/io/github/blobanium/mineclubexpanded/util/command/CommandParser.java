b9588ba64b45d13d3a698d2b451f7d04bfe8a48f
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-01-18 16:01:14 -0800

    Add "mcex" command for commands list

--- src/main/java/io/github/blobanium/mineclubexpanded/util/command/CommandParser.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/command/CommandParser.java
@@ -1,14 +1,20 @@
 package io.github.blobanium.mineclubexpanded.util.command;
 
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.item.Item;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 
 import java.text.NumberFormat;
 
 public class CommandParser {
     public static void registerCommand() {
+        ClientCommandManager.DISPATCHER.register(
+                ClientCommandManager.literal("mcex").executes(context -> {
+                    context.getSource().sendFeedback(Text.Serializer.fromJson("[\"\",{\"text\":\"Mineclub Expanded\\n\"},{\"text\":\"Click here to see the full set of commands\",\"color\":\"light_purple\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://github.com/Blobanium/mineclub-expanded/wiki/List-Of-Commands\"}}]"));
+                    return 0;
+                })
+        );
+
         ClientCommandManager.DISPATCHER.register(
                 ClientCommandManager.literal("mcex").then(ClientCommandManager.literal("getminingvalue").executes(context -> {
                     context.getSource().sendFeedback(new LiteralText("§dCurrent value: §r" + NumberFormat.getInstance().format(Commands.getOreValue())));
