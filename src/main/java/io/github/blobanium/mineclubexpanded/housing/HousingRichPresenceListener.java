affe5790a4a517016f2aa927ff0704e1b114ebcc
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-02-11 16:46:09 -0800

    Rework HousingRichPresenceListener

--- src/main/java/io/github/blobanium/mineclubexpanded/housing/HousingRichPresenceListener.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/housing/HousingRichPresenceListener.java
@@ -11,21 +11,18 @@ public class HousingRichPresenceListener {
 
     public static void sendHousingPresence(boolean fromServerIP){
         if(fromServerIP){
-            DiscordRP.updateStatus("Currently In " + usernameFromServerIP + "'s Home", DiscordRP.defaultDetails());
-            checkBlobaniumsHome(usernameFromServerIP);
+            DiscordRP.updateStatus(rpHousingState(usernameFromServerIP), DiscordRP.defaultDetails());
         } else {
             String message = MineclubExpanded.lastChatField;
             if (message.startsWith("/home")) {
                 String finalMessage = message.replaceAll("/home", "");
                 if (finalMessage.equals("")) {
-                    DiscordRP.updateStatus("Currently In " + MinecraftClient.getInstance().getSession().getUsername() + "'s Home", DiscordRP.defaultDetails());
+                    DiscordRP.updateStatus(rpHousingState(MinecraftClient.getInstance().getSession().getUsername()), DiscordRP.defaultDetails());
                 } else {
-                    DiscordRP.updateStatus("Currently In " + finalMessage + "'s Home", DiscordRP.defaultDetails());
-                    checkBlobaniumsHome(finalMessage);
+                    DiscordRP.updateStatus(rpHousingState(finalMessage), DiscordRP.defaultDetails());
                 }
             } else if (playerheadName != null) {
-                DiscordRP.updateStatus("Currently In " + playerheadName + "'s Home", DiscordRP.defaultDetails());
-                checkBlobaniumsHome(playerheadName);
+                DiscordRP.updateStatus(rpHousingState(playerheadName), DiscordRP.defaultDetails());
             } else {
                 DiscordRP.updateStatus("Currently In Housing", DiscordRP.defaultDetails());
             }
@@ -33,9 +30,10 @@ public class HousingRichPresenceListener {
         }
     }
 
-    private static void checkBlobaniumsHome(String username){
+    private static String rpHousingState(String username){
         if(username.matches("(B|b)lobanium")){
             MinecraftClient.getInstance().player.sendMessage(Text.Serializer.fromJson("[\"\",{\"text\":\"Welcome to my house! Do you like my housing? \"},{\"text\":\"Click here to like!\",\"color\":\"light_purple\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/like\"}}]"), false);
         }
+        return "Currently In " + username + "'s Home";
     }
 }
