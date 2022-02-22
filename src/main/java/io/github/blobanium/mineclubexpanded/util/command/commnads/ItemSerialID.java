777e37370244b50443f3d72ff7510105c71922f3
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-02-21 19:07:16 -0800

    Update ItemSerialID.java

--- src/main/java/io/github/blobanium/mineclubexpanded/util/command/commnads/ItemSerialID.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/command/commnads/ItemSerialID.java
@@ -1,8 +1,10 @@
 package io.github.blobanium.mineclubexpanded.util.command.commnads;
 
+import io.github.blobanium.mineclubexpanded.global.WorldID;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.collection.DefaultedList;
 
 import java.util.UUID;
@@ -10,8 +12,24 @@ import java.util.UUID;
 public class ItemSerialID {
     public static String getItemSerial(){
         PlayerInventory currentinv = MinecraftClient.getInstance().player.getInventory();
-        String clubId = currentinv.main.get(currentinv.selectedSlot).getNbt().getCompound("PublicBukkitValues").getString("mineclub-lobby:club_id");
+        NbtCompound nbt = currentinv.main.get(currentinv.selectedSlot).getNbt();
+        String clubId = nbt.getCompound("PublicBukkitValues").getString(getKey());
+        if(nbt == null){
+            return "";
+        }else if(clubId.equals("MINECOIN")){
+            return "";
+        }else if(clubId.equals("")){
+            return "Your phone doesnt have a serial number";
+        }
         int serial = Math.abs(UUID.fromString(clubId).hashCode());
         return "#" + serial;
+    }
+
+    private static String getKey(){
+        if(WorldID.worldID == WorldID.HOUSING){
+            return "mineclubhousing:club_id";
+        }else{
+            return "mineclub-lobby:club_id";
+        }
     }
 }
