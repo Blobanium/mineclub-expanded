487aab627075b4fc25a3a0f04cfc3b9d0f47e48d
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-03-18 20:53:13 -0700

    Remove unnecessary class

--- src/main/java/io/github/blobanium/mineclubexpanded/mixin/DisconnectedScreenMixin.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/mixin/DisconnectedScreenMixin.java
@@ -1,6 +1,8 @@
 package io.github.blobanium.mineclubexpanded.mixin;
 
-import io.github.blobanium.mineclubexpanded.util.mixinhelper.AutoReconnectManager;
+import io.github.blobanium.mineclubexpanded.MineclubExpanded;
+import io.github.blobanium.mineclubexpanded.util.config.ConfigReader;
+import io.github.blobanium.mineclubexpanded.util.feature.Autoreconnect;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -11,9 +13,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DisconnectedScreen.class)
 public class DisconnectedScreenMixin {
+    private static int attempts = 0;
 
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(Screen parent, Text title, Text reason, CallbackInfo ci) {
-        AutoReconnectManager.onDisconnect(parent, title, reason);
+        onDisconnect(parent, title, reason);
+    }
+
+    private static void onDisconnect(Screen parent, Text title, Text reason) {
+        MineclubExpanded.LOGGER.warn("Disconnection occurred.\nTitle=" + title.getString() + "\nReason=" + reason.getString());
+        if (ConfigReader.autoReconnect && !wasUsingMaliciousIntent(reason) && attempts <= ConfigReader.autoReconnectAttempts) {
+            Autoreconnect.setReminder(ConfigReader.autoReconnectSeconds, parent);
+            attempts = attempts + 1;
+        } else if (wasUsingMaliciousIntent(reason)){
+            MineclubExpanded.LOGGER.error( "Autoreconnect will not reconnect to Mineclub.\nDo not use Autoreconnect with malicious intent.");
+        }
+    }
+
+    private static boolean wasUsingMaliciousIntent(Text reason){
+        if (reason.getString().contains("You were kicked for")) {
+            return true;
+        }else if (reason.getString().contains("You were banned for")) {
+            return true;
+        }else return reason.getString().contains("You are banned for");
     }
 }
