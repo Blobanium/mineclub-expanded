bf491f966327a4cf213f27e4a63ce5bc37941476
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-03-31 17:44:01 -0700

    hypixel expanded

--- src/main/java/io/github/blobanium/mineclubexpanded/util/mixinhelper/InventoryItemIdentifier.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/util/mixinhelper/InventoryItemIdentifier.java
@@ -1,6 +1,8 @@
 package io.github.blobanium.mineclubexpanded.util.mixinhelper;
 
 import io.github.blobanium.mineclubexpanded.housing.HousingRichPresenceListener;
+import io.github.blobanium.mineclubexpanded.util.AprilFoolsException;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
