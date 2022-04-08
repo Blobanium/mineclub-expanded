bacabd368545f115b3eec242ecbc5ef5ad713922
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-04-08 15:28:23 -0700

    add when to update presence (not integrated yet)

--- src/main/java/io/github/blobanium/mineclubexpanded/games/BattleDomePresence.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/games/BattleDomePresence.java
@@ -6,6 +6,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 public class BattleDomePresence {
+    public static OffsetDateTime offsetlastfield = null;
 
     public static OffsetDateTime getTimeLeft(String bossbartext, OffsetDateTime time){
         ArrayList<String> array = UnicodeTranslator.decodeToArrayList(bossbartext);
@@ -20,8 +21,20 @@ public class BattleDomePresence {
     }
 
     public static OffsetDateTime getTimeLeft(String bossbartext){
-        return getTimeLeft(bossbartext, OffsetDateTime.now());
+        OffsetDateTime value = getTimeLeft(bossbartext, OffsetDateTime.now());
+        if(offsetlastfield != null){
+            if(compare(offsetlastfield, value) >= 1000000000L){
+                System.out.println("Update Offset Time presence");
+            }
+        }
+        offsetlastfield = value;
+        return value;
     }
-
-
+    private static long compare(OffsetDateTime date1, OffsetDateTime date2){
+        long hoursDiff = date2.getHour() - date1.getHour();
+        long minutesDiff = date2.getMinute() - date1.getMinute();
+        long secondsDiff = date2.getSecond() - date1.getSecond();
+        long nanoDiff = date2.getNano() - date1.getNano();
+        return nanoDiff + (secondsDiff * 1000000000L) + (minutesDiff * 60000000000L) + (hoursDiff * 3600000000000L);
+    }
 }
