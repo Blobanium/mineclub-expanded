81deafde089b811d697cc5d212d9fe3f63743bf0
Blobanium <45613796+Blobanium@users.noreply.github.com>
2022-04-16 15:24:53 -0700

    fix outbid notification not working

--- src/main/java/io/github/blobanium/mineclubexpanded/market/OutbidNotifier.java
+++ src/main/java/io/github/blobanium/mineclubexpanded/market/OutbidNotifier.java
@@ -7,7 +7,7 @@ import net.minecraft.text.Text;
 public class OutbidNotifier {
     public static void onChatMessage(Text message){
         String textMessage = message.getString().replaceAll("\\[","\\\\[");
-        if(textMessage.startsWith("ꌄ§8\\[§dMarket§8] §cYou have been outbid by")){
+        if(textMessage.startsWith("ꌄ冈 §8\\[§dMarket§8] §cYou have been outbid by")){
             SoundPlayer.playSound(1.0F, SoundEvents.ENTITY_BEE_DEATH);
         }
         if(textMessage.startsWith("ꌄ§8\\[§dHousing§8]§c You have been outbid by")){
